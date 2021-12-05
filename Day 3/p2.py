e2ffe35451837f48b0d367bff7aec2776f32fb3d
nelup20 <platon.nelu@yahoo.com>
2021-12-05 19:26:32 +0100

    Day 3 part 2 refactored

--- Day 3/p2.py
+++ Day 3/p2.py
@@ -1,61 +1,41 @@
 
-oxygen_gen_rating = ""
-co2_scrub_rating = ""
-count_zero = 0
-count_one = 0
+import enum
+
+class Type(enum.Enum):
+    Oxygen = 0
+    Co2 = 1
 
 with open("./input.txt") as file:
     original_arr = file.readlines()
 
-filtered_arr = original_arr
+def findResultForType(inputArr, type):
+    result = ""
+    count_zero = 0
+    count_one = 0
 
-for i in range(12):
-    if(len(filtered_arr) > 2):
-        for line in filtered_arr:
-            if (line[i] == "0"):
-                count_zero += 1
+    for i in range(12):
+        if (len(inputArr) > 2):
+            for line in inputArr:
+                if (line[i] == "0"):
+                    count_zero += 1
+                else:
+                    count_one += 1
+
+            if (count_zero > count_one):
+                result += "0" if type == Type.Oxygen else "1"
             else:
-                count_one += 1
+                result += "1" if type == Type.Oxygen else "0"
 
-        if (count_zero > count_one):
-            oxygen_gen_rating += "0"
+            inputArr = list(filter(lambda x: x.startswith(result), inputArr))
+            count_one = 0
+            count_zero = 0
         else:
-            oxygen_gen_rating += "1"
-
-        filtered_arr = list(filter(lambda x: x.startswith(oxygen_gen_rating), filtered_arr))
-        count_one = 0
-        count_zero = 0
-    else:
-        if(filtered_arr[0][i] == "1"):
-            oxygen_gen_rating = filtered_arr[0] + ""
-        else:
-            oxygen_gen_rating = filtered_arr[1] + ""
-        break
-
-filtered_arr = original_arr
-
-# Too lazy to refactor right now...
-for i in range(12):
-    if(len(filtered_arr) > 2):
-        for line in filtered_arr:
-            if (line[i] == "0"):
-                count_zero += 1
+            if(type == Type.Oxygen):
+                result = inputArr[0] if inputArr[0][i] == "1" else inputArr[1]
             else:
-                count_one += 1
+                result = inputArr[0] if inputArr[0][i] == "0" else inputArr[1]
+            break
 
-        if (count_zero > count_one):
-            co2_scrub_rating += "1"
-        else:
-            co2_scrub_rating += "0"
+    return result
 
-        filtered_arr = list(filter(lambda x: x.startswith(co2_scrub_rating), filtered_arr))
-        count_one = 0
-        count_zero = 0
-    else:
-        if(filtered_arr[0][i] == "0"):
-            co2_scrub_rating = filtered_arr[0] + ""
-        else:
-            co2_scrub_rating = filtered_arr[1] + ""
-        break
-
-print(int(oxygen_gen_rating, 2) * int(co2_scrub_rating, 2))
+print(int(findResultForType(original_arr, Type.Oxygen), 2) * int(findResultForType(original_arr, Type.Co2), 2))
