a40099472c439cfddadb9975eca2a0686da2cd08
nelup20 <platon.nelu@yahoo.com>
2021-12-19 01:56:39 +0100

    Day 4 part 2 done ✅

---
+++ Day 4/p2.rb
@@ -0,0 +1,63 @@
+
+numbers = File.read('numbers.txt').split(',').map(&:to_i)
+
+boards = File.read('boards.txt').split("\n")
+boards.delete_if { |x| x == '' }
+boards = boards.map { |x| x.split(' ').map(&:to_i) }
+boards = boards.each_slice(5).entries
+
+def bingo?(board_to_check)
+  row_nil = 0
+  col_nil = 0
+
+  (0..4).each do |i|
+    (0..4).each do |j|
+      row_nil += 1 if board_to_check[i][j].nil?
+
+      col_nil += 1 if board_to_check[j][i].nil?
+    end
+
+    return true if row_nil == 5 || col_nil == 5
+
+    row_nil = 0
+    col_nil = 0
+  end
+
+  false
+end
+
+def mark_board(board, number)
+  board.each { |row| row.map! { |num| num == number ? nil : num } }
+end
+
+def find_winning_board(numbers, boards)
+  winning_boards = []
+  winning_numbers = []
+  
+  (0..numbers.length - 1).each do |i|
+    (0..boards.length - 1).each do |j|
+      next if winning_boards.include?(j)
+
+      mark_board(boards[j], numbers[i])
+
+      next unless bingo?(boards[j])
+
+      winning_boards.push(j)
+      winning_numbers.push(i)
+    end
+  end
+
+  [boards[winning_boards[-1]], numbers[winning_numbers[-1]]]
+end
+
+winning_board, last_number = find_winning_board(numbers, boards)
+result = 0
+
+winning_board.each { |row| row.each { |num| result += num unless num.nil? } }
+
+result *= last_number
+
+puts "Final result: #{result}"
+
+
+
