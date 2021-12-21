d90d8be66238c7a39176a481bf88ebae9c346bd4
nelup20 <platon.nelu@yahoo.com>
2021-12-22 00:12:24 +0100

    Day 7 part 1 done ✅

---
+++ Day 7/p1/p1/P1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace p1
+{
+    class P1
+    {
+        static void Main(string[] args)
+        {
+            int[] input = System.IO.File.ReadAllText("../../../input.txt").Split(",").Select(int.Parse).ToArray();
+            Array.Sort(input);
+
+            int median = input[input.Length / 2];
+            int result = 0;
+
+            foreach (var elem in input)
+            {
+                result += Math.Abs(median - elem);
+            }
+            
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
